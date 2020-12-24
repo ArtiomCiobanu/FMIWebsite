f5e11da3617c2acbfab47cf7613106c8beceaf15
Artiom Ciobanu <aciobanumain@gmail.com>
2020-12-24 23:04:21 +0200

    Development (#6)
    * Added ClaimsPrincipalExtensions class
    
    * .
    
    * UserId type changed to Guid

--- FMIWebsiteAPI.JwtAuthorization/API/IJwtManager.cs
+++ FMIWebsiteAPI.JwtAuthorization/API/IJwtManager.cs
@@ -1,9 +1,12 @@
+using System;
+using FMIWebsiteAPI.Models.Accounts;
+
 namespace FMIWebsiteAuthorizationAPI.API
 {
     public interface IJwtManager
     {
         IJwtConfigurator GetJwtConfigurator();
-        string GenerateToken(string userId, string userRole);
+        string GenerateToken(Guid userId, UserRole userRole);
         string GetUserIdFromToken(string token);
         string GetUserRoleFromToken(string token);
         string GetUserDataFromToken(string token, string claimType);
