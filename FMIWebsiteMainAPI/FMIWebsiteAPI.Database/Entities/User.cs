f01d72e0014235f5c08da0e5e81dea455136ada3
Artiom Ciobanu <aciobanumain@gmail.com>
2020-12-28 22:18:37 +0200

    Added UnitTests and Database projects

---
+++ FMIWebsiteMainAPI/FMIWebsiteAPI.Database/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FMIWebsiteAPI.Models.Accounts;
+
+namespace FMIWebsiteAPI.Database.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public UserRole Role { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ FMIWebsiteMainAPI/FMIWebsiteAPI.Database/MainContext.cs
@@ -0,0 +1,10 @@
+﻿using FMIWebsiteAPI.Models.Accounts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FMIWebsiteAPI.Database
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
