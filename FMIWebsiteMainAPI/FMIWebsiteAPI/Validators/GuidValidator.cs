b3f0f33f8d6f8b9c5ec83e6ffc7391e5a8d260ad
Artiom Ciobanu <aciobanumain@gmail.com>
2021-01-03 19:14:32 +0200

    Added Fluent Validation (#16)
    * Added fluent validation

---
+++ FMIWebsiteMainAPI/FMIWebsiteAPI/Validators/GuidValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+
+namespace FMIWebsiteAPI.Validators
+{
+    public class GuidValidator : AbstractValidator<Guid>
+    {
+        public GuidValidator()
+        {
+            RuleFor(guid => guid)
+                .NotEmpty()
+                .WithMessage("Guid must not be empty");
+        }
+    }
+}
