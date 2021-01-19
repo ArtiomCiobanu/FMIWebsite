fbb7d304b972f6f4ee208e05bd1b0e4e3db38385
Artiom Ciobanu <aciobanumain@gmail.com>
2021-01-19 21:02:13 +0200

    Added Email DataType attribute to Email

--- NewsWebsiteMainAPI/NewsWebsiteAPI.DataAccess/Entities/Account.cs
+++ NewsWebsiteMainAPI/NewsWebsiteAPI.DataAccess/Entities/Account.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using NewsWebsiteAPI.Infrastructure.Enums;
 
 namespace NewsWebsiteAPI.DataAccess.Entities
@@ -8,6 +9,8 @@ namespace NewsWebsiteAPI.DataAccess.Entities
         public Guid Id { get; set; }
         public UserRole Role { get; set; }
         public string FullName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
     }
 }
