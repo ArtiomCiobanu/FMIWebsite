540e7eb4c211e2fc31955e9c8af5f4b30727034f
Artiom Ciobanu <aciobanumain@gmail.com>
2021-01-19 20:00:25 +0200

    Little changes

--- NewsWebsiteMainAPI/NewsWebsiteAPI.DataAccess/Entities/Account.cs
+++ NewsWebsiteMainAPI/NewsWebsiteAPI.DataAccess/Entities/Account.cs
@@ -3,10 +3,11 @@ using NewsWebsiteAPI.Infrastructure.Enums;
 
 namespace NewsWebsiteAPI.DataAccess.Entities
 {
-    public class User
+    public class Account
     {
         public Guid Id { get; set; }
         public UserRole Role { get; set; }
         public string FullName { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ NewsWebsiteMainAPI/NewsWebsiteAPI.DataAccess/EntityConfigurations/AccountConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewsWebsiteAPI.DataAccess.Entities;
+
+namespace NewsWebsiteAPI.DataAccess.EntityConfigurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder
+                .HasKey(account => account.Id);
+
+            builder
+                .Property(account => account.Role)
+                .IsRequired();
+            builder
+                .Property(account => account.FullName)
+                .IsRequired();
+        }
+    }
+}
