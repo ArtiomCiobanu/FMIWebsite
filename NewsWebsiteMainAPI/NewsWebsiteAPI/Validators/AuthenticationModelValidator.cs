be2625e549eceffc89dbd4fc3b3ac455eb4d385d
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-03-01 16:06:39 +0200

    Added model validators

---
+++ NewsWebsiteMainAPI/NewsWebsiteAPI/Validators/AuthenticationModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using NewsWebsiteAPI.Models.Dto.Accounts;
+
+namespace NewsWebsiteAPI.Validators
+{
+    public class AuthenticationModelValidator : AbstractValidator<AuthenticationModel>
+    {
+        public AuthenticationModelValidator()
+        {
+            RuleFor(am => am.Email)
+                .NotEmpty()
+                .MaximumLength(50)
+                .EmailAddress()
+                .WithMessage("Email is not valid.");
+
+            RuleFor(am => am.Password)
+                .NotEmpty()
+                .MaximumLength(50)
+                .WithMessage("Password is not valid");
+        }
+    }
+}
